65dab84065f1f74539d18fa78ffd930654650f7e
sc.hwang <sc.hwang@sk.com>
2016-01-26 10:23:51 +0900

    real test

--- src/main/java/io/circleline/Application.java
+++ src/main/java/io/circleline/Application.java
@@ -1,26 +1,32 @@
 package io.circleline;
 
-import akka.actor.ActorSystem;
+import io.circleline.router.RestAPIRouter;
 import org.apache.camel.CamelContext;
 import org.apache.camel.impl.DefaultCamelContext;
-import org.apache.camel.main.Main;
 
 /**
  * Created by 1001923 on 16. 1. 14..
  */
 public class Application {
     public static void main(String[] args) throws Exception {
-        Main main = new Main();
-        main.enableHangupSupport();
-        main.run();
+        final Configuration config = new Configuration();
+        RestAPIRouter sut = new RestAPI(config).restAPIRouter();
+        CamelContext context = new DefaultCamelContext();
+
+        context.addRoutes(sut);
+        context.start();
+
+//        Main main = new Main();
+//        main.enableHangupSupport();
+//        main.run();
 //        run();
     }
 
-    public static void run() throws Exception{
-        ActorSystem system = ActorSystem.create("MyActorSystem");
-        CamelContext context = new DefaultCamelContext();
-
-        context.start();
+//    public static void run() throws Exception{
+//        ActorSystem system = ActorSystem.create("MyActorSystem");
+//        CamelContext context = new DefaultCamelContext();
+//
+//        context.start();
 
 
 
@@ -28,7 +34,7 @@ public class Application {
 //        pingActor.tell(new PingActor.Initialize(), null);
         // This example app will ping pong 3 times and thereafter terminate the ActorSystem -
         // see counter logic in PingActor
-        system.awaitTermination();
-        context.stop();
-    }
+//        system.awaitTermination();
+//        context.stop();
+//    }
 }
--- src/test/java/io/circleline/RestAPIRouterTest.java
+++ src/test/java/io/circleline/RestAPIRouterTest.java
@@ -2,6 +2,7 @@ package io.circleline;
 
 import io.circleline.router.RestAPIRouter;
 import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.junit.Test;
 
@@ -19,8 +20,11 @@ public class RestAPIRouterTest {
         context.addRoutes(sut);
         context.start();
 
+        final ProducerTemplate producerTemplate = context.createProducerTemplate();
+        final String result = producerTemplate.requestBody("jetty:http://0.0.0.0:8080/acme/ping", null,
+                String.class);
 
         //then
-        context.stop();
+//        context.stop();
     }
 }
