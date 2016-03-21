122a0d3edb4b7accf31813a03dae5719a92cfb3f
bruce shin <ssshow16@gmail.com>
2016-03-21 09:31:51 +0900

    REST API. 1.KEY 관리 추가(프로토타입)

---
+++ src/main/java/io/circleline/dao/KeysDao.java
@@ -0,0 +1,12 @@
+package io.circleline.dao;
+
+import io.circleline.domain.KeyInfo;
+
+/**
+ * Created by 1002515 on 2016. 3. 18..
+ */
+public interface KeysDao {
+
+    void createKey(KeyInfo keyInfo);
+
+}
