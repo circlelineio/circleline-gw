77a28c0ebe1eed7492103acc52a04d15bc17406b
bruce shin <ssshow16@gmail.com>
2016-03-21 09:56:33 +0900

    lombok으로 변경

--- src/main/java/io/circleline/domain/KeyInfo.java
+++ src/main/java/io/circleline/domain/KeyInfo.java
@@ -3,38 +3,14 @@ package io.circleline.domain;
 /**
  * Created by 1002515 on 2016. 3. 18..
  */
+import lombok.Data;
+
+@Data
 public class KeyInfo {
 
     private String apiId;
 
     public KeyInfo(String apiId) {
         this.apiId = apiId;
-    }
-
-    public String getApiId() {
-        return apiId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        KeyInfo keyInfo = (KeyInfo) o;
-
-        return !(apiId != null ? !apiId.equals(keyInfo.apiId) : keyInfo.apiId != null);
-
-    }
-
-    @Override
-    public int hashCode() {
-        return apiId != null ? apiId.hashCode() : 0;
-    }
-
-    @Override
-    public String toString() {
-        return "KeyInfo{" +
-                "apiId='" + apiId + '\'' +
-                '}';
     }
 }
--- src/main/java/io/circleline/domain/KeyStatus.java
+++ src/main/java/io/circleline/domain/KeyStatus.java
@@ -1,5 +1,8 @@
 package io.circleline.domain;
 
+import lombok.Data;
+
+@Data
 public class KeyStatus {
     private String key;
     private String status;
@@ -9,47 +12,5 @@ public class KeyStatus {
         this.key = key;
         this.status = status;
         this.action = action;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public String getAction() {
-        return action;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        KeyStatus keyStatus = (KeyStatus) o;
-
-        if (key != null ? !key.equals(keyStatus.key) : keyStatus.key != null) return false;
-        if (status != null ? !status.equals(keyStatus.status) : keyStatus.status != null) return false;
-        return !(action != null ? !action.equals(keyStatus.action) : keyStatus.action != null);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = key != null ? key.hashCode() : 0;
-        result = 31 * result + (status != null ? status.hashCode() : 0);
-        result = 31 * result + (action != null ? action.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "KeyStatus{" +
-                "key='" + key + '\'' +
-                ", status='" + status + '\'' +
-                ", action='" + action + '\'' +
-                '}';
     }
 }
